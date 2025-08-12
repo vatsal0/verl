29b221f16977d4df150834cf9c0d0a08b2325850
vatsal0 <vatsalbaherwani@gmail.com>
2025-08-12 00:34:55 +0000

    setup verison fix

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@ conda create -n verl python==3.10 -y
 conda activate verl
 USE_MEGATRON=0 bash scripts/install_vllm_sglang_mcore.sh
 
-pip install opentelemetry-api==1.36.0
-pip install opentelemetry-sdk==1.36.0
+pip install opentelemetry-api==1.26.0
+pip install opentelemetry-sdk==1.26.0
 pip install --no-deps -e .
 
 python examples/data_preprocess/gsm8k.py --local_dir ~/data/gsm8k
